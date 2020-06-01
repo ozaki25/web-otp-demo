19570b291c5ae6faf213a5ddfd2257d4774ddb1b
ozaki <fs65ugumori@yahoo.co.jp>
2020-06-02 00:29:11 +0900

    add send sms function

--- handler.ts
+++ handler.ts
@@ -1,12 +1,46 @@
+import { SNS } from 'aws-sdk';
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
 
-export const hello: APIGatewayProxyHandler = async (event, _context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      input: event,
-    }, null, 2),
-  };
-}
+type bodyType = {
+  message?: string;
+  phoneNumber?: string;
+};
+
+const sns = new SNS();
+
+export const sendSMS: APIGatewayProxyHandler = async event => {
+  const { body } = event;
+  console.log({ body });
+
+  if (!body) return { statusCode: 500, body: 'body is empty' };
+
+  const { message, phoneNumber }: bodyType = JSON.parse(body);
+  console.log({ message, phoneNumber });
+
+  if (!message || !phoneNumber) {
+    return {
+      statusCode: 500,
+      body: `invalid params. message: ${message}, phoneNumber: ${phoneNumber}`,
+    };
+  }
+
+  try {
+    const params: SNS.Types.PublishInput = {
+      Message: message,
+      PhoneNumber: `+81${phoneNumber.slice(1)}`,
+    };
+    const result = await sns.publish(params).promise();
+    console.log(result);
+    return {
+      statusCode: 200,
+      body: 'Hello',
+    };
+  } catch (e) {
+    console.log(e);
+    return {
+      statusCode: 500,
+      body: JSON.stringify(e),
+    };
+  }
+};
