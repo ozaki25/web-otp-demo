c15eb2af50fae078590ec5019bf8db9221d88172
ozaki <fs65ugumori@yahoo.co.jp>
2020-06-06 00:47:51 +0900

    apply router

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Router from './routes/router';
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <h1>Web Otp Demo</h1>
+      <Router />
     </div>
   );
 }
